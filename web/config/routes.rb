74740d868533f64b0d1c2e4778e100b6c9f14a42
Max <upc.ingenieria2015@gmail.com>
2016-07-11 02:07:05 +0000

    perfil y recovery

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -9,19 +9,11 @@ Rails.application.routes.draw do
 
   get 'contact/contactenos'
 
-<<<<<<< HEAD
   get 'home/index'
 
   get 'home/about'
   get "home/recuperar_contrasena"
 
-=======
-
-  get 'home/index'
-
-  get 'home/about'
-  
->>>>>>> bd76ac4394fb641671d2d61a22f16300d2afba50
   get 'home/testimonio'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
