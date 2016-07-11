9288b118daca538a316872b266aa0774c049b640
jona2016 <jonathan.sucasaca@gmail.com>
2016-07-11 02:02:49 +0000

    Correcion2

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,20 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
   get 'account/login'
 
   get 'account/crear_cuenta'
 
   get 'account/recuperar_cuenta'
-=======
-=======
-  get 'parkin/nuevo'
-
->>>>>>> ab33a9d2c6ebf931d4747c451ff65c1580c369e4
   get 'contact/contactenos'
 
 
->>>>>>> 4716046213d4d9543308a4c9b12fc3e4a9919191
 
   get 'home/index'
 
@@ -22,6 +16,8 @@ Rails.application.routes.draw do
   
   get 'home/testimonio'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
+  
   root 'home#index'
+  
 end
