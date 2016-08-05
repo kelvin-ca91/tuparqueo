a75b8ed983b8e07cc80e1d32de47dd0964bfe478
Kelvin <kelvin.ca91@gmail.com>
2016-08-05 07:43:58 +0000

    Formulario de parkings con mapa y cordenadas

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801213055) do
+ActiveRecord::Schema.define(version: 20160804072314) do
+
+  create_table "districts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nombre"
+    t.string   "lat_centro"
+    t.string   "lon_centro"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "users_id"
