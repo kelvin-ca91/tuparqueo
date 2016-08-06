5b2f30d71fc6ee9e4106d90899b158377cd6ada6
rbert1903 <rbert3@gmail.com>
2016-08-06 05:56:55 +0000

    monto total

--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -17,6 +17,8 @@ class HomeController < ApplicationController
       @parking = Parking.find_by_sql("SELECT * FROM parkings WHERE users_id = #{current_user.id} AND estado!=0 ")
       @favorite = Favorite.find_by_sql("SELECT p.*, f.id as favorite_id FROM parkings p JOIN favorites f ON f.parkings_id = p.id AND p.estado!= 0 AND f.users_id= #{current_user.id}")
       @rentals = Rental.find_by_sql("SELECT p.*, r.id AS rentals_id FROM parkings p LEFT JOIN rentals r ON r.parkings_id = p.id AND r.estado = 1 AND r.users_id = #{current_user.id}")
+      @monto = Rental.find_by_sql("SELECT SUM(r.monto) AS monto_total FROM rentals r LEFT JOIN parkings p ON r.parkings_id = p.id AND p.users_id = #{current_user.id}")
+      
     end
   end
   
