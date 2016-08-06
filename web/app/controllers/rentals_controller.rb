04a742badb38a07d5d5ae61d80ead8e1d3170e86
rbert1903 <rbert3@gmail.com>
2016-08-06 23:13:29 +0000

    eliminar alquiler

--- web/app/controllers/rentals_controller.rb
+++ web/app/controllers/rentals_controller.rb
@@ -30,7 +30,9 @@ class RentalsController < ApplicationController
     end
     
     def destroy
-        
+        rentals = Rentals.find_by(id: params[:id])
+        rentals.update(estado: 0)
+        # Parking.find(params[:id]).destroy
     end
     
     
